cacc5d31c8e977acc8a54fcb57a8e01ee6fd4444
Sevval-Yildiz <yildizsevval76@gmail.com>
2025-12-28 16:19:44 +0300

    Transferable interface ine Javadoc eklendi.

--- src/Transferable.java
+++ src/Transferable.java
@@ -1,3 +1,9 @@
 public interface Transferable {
+    /**
+     * Başka bir hesaba para transferi yapmamızı sağlayan interface.
+     *  @param toAccount Paranın gönderileceği hesap.
+     * @param amount Gönderilecek para miktarı.
+     * @return İşlem başaralıysa true işlem başarısızsa false döner.
+     */
     boolean transfer (Account toAccount , double amount);
 }
