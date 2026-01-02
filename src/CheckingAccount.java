bbee1810ac150c659de2c4f037e7af581c2114bd
Sevval-Yildiz <yildizsevval76@gmail.com>
2026-01-02 18:12:23 +0300

    Vadesiz hesap sınıfı oluşturuldu ve vadesiz hesap sınfına özel withdraw metodu eklendi.

---
+++ src/CheckingAccount.java
@@ -0,0 +1,25 @@
+public class CheckingAccount extends Account {
+    private double overdraftLimit; // Kredi limiti. Eksiye düşme hakkı.
+
+    public CheckingAccount(String accountNumber, String ownerName, double initialBalance, double overdraftLimit) {
+        super(accountNumber, ownerName, initialBalance);
+        this.overdraftLimit = overdraftLimit;
+    }
+    @Override
+    public boolean withdraw(double amount){
+        /* Vadesiz hesaplarda insanlar mevcut parası + kredi limiti kadar para çekebilir yani eksiye düşebilir.
+        Ancak para + kredi limitinden fazlasını çekemezler.
+         */
+        if(this.balance + this.overdraftLimit >= amount){
+            this.balance -= amount;
+            addTransaction("Çekme", amount, "Vadesiz hesaptan çekildi.");
+            return true;
+        }else{
+            System.out.println("Yetersiz bakiye ve kredi limiti aşımı !");
+            return false;
+        }
+    }
+    public double getOverdraftLimit(){ // Limiti öğrenmek için getter.
+        return overdraftLimit;
+    }
+}
