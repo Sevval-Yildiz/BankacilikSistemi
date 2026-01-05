c71b2a51a2aebe8b2398913a3f650ef759139a20
Sevval-Yildiz <yildizsevval76@gmail.com>
2026-01-06 02:29:15 +0300

    BankTest sınıfı eklendi.

---
+++ src/BankTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BankTest {
+
+    @Test
+    void testHesapEklemeVeBulma() {
+        Bank banka = new Bank();
+        // Test için sanal bir hesap oluşturuyoruz
+        SavingsAccount hesap = new SavingsAccount("10", "Test Kullanıcısı", 5000.0, 10.0);
+
+        // Hesabı bankaya ekliyoruz
+        banka.addAccount(hesap);
+
+        // Bakalım bankadan geri isteyince doğru geliyor mu?
+        Account bulunan = banka.getAccount("10");
+
+        assertNotNull(bulunan); // "Null gelmesin, dolu gelsin" demek
+        assertEquals("Test Kullanıcısı", bulunan.getOwnerName()); // İsim doğru mu?
+    }
+
+    @Test
+    void testOlmayanHesap() {
+        Bank banka = new Bank();
+
+        // Banka boş, ama biz "999" nolu hesabı istiyoruz
+        Account sonuc = banka.getAccount("11");
+
+        // Beklentimiz: Böyle bir hesap olmadığı için NULL gelmesi
+        assertNull(sonuc);
+    }
+}
