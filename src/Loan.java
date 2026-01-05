4eb544fb84a533327370e6d2ec5f8d245d6b019f
Sevval-Yildiz <yildizsevval76@gmail.com>
2026-01-05 14:39:04 +0300

    Loan sınıfına calculateLoan metodu eklendi.

--- src/Loan.java
+++ src/Loan.java
@@ -27,4 +27,26 @@ public class Loan {
         System.out.println("Aylık Taksit : " + calculateMonthlyPayment() + "TL");
         System.out.println("Toplam Geri Ödeme : " + getTotalPayment() + "TL");
     }
+
+    public static void calculateLoan(int krediTuru, double anaPara, int vade) {
+        double oran = 0;
+        String tur = "";
+
+        if (krediTuru == 1) {
+            oran = 0.20; // İhtiyaç kredisi.
+            tur = "İhtiyaç Kredisi";
+        } else if (krediTuru == 2) {
+            oran = 0.15; // Taşıt kredisi.
+            tur = "Taşıt Kredisi";
+        } else if (krediTuru == 3) {
+            oran = 0.10; // Konut kredisi.
+            tur = "Konut Kredisi";
+        } else {
+            System.out.println("Hatalı seçim yaptınız!");
+            return;
+        }
+
+        Loan yeniKredi = new Loan(anaPara, vade, oran, tur);
+        yeniKredi.printLoanDetails();
+    }
 }
